d5ef201d38e7878bca058c9890c4f08bf8c8a1e7
yang <aetheryang@gmail.com>
2014-09-13 18:11:43 +0000

    modified:   conjugation.rb           copyright

--- conjugation.rb
+++ conjugation.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/ruby
-#This program is a deamo to recite word
+#
+# This program is a deamo to recite word
+# Author  : aetheryang
+# Mail    : aetheryang@gmail.com
+# Copyright (c) 2014 aetheryang
+#
 require_relative "phrase.rb"
 require_relative "control.rb"
-
 
 app = Control.new
 loop do
